9e76d036768e66a33e3102091faffc11d0a5b604
supermomonga <hi@supermomonga.com>
2014-11-22 15:42:39 +0900

    fix debug

--- plugins/notifications.rb
+++ plugins/notifications.rb
@@ -21,9 +21,11 @@ module Notifications
     text = "[BED] #{player.name}さんがベッドに横たわっておられる"
 
     later sec(0.5) do
-      awake_players = Bukkit.online_players.to_a.tap{|pl|
-        p pl
-        p pl.sleeping?
+      awake_players = Bukkit.online_players.to_a.tap{|pls|
+        pls.each do |pl|
+          p pl
+          p pl.sleeping?
+        end
       }.reject(&:sleeping?).map(&:name)
       unless awake_players.empty?
         text += " (#{awake_players.join ' '}達は今すぐ寝#{%w[ましょう ろ んかい].sample})"
