f9a7863e716f187db08ba405517e33e27690cfbd
ujihisa <ujihisa@gmail.com>
2014-11-23 01:50:51 -0800

    why it was each_with_object?

--- plugins/chat_relay.rb
+++ plugins/chat_relay.rb
@@ -152,7 +152,7 @@ module ChatRelay
         evt.message.tr(*KANA_CONVERSION_TABLE).split
       else
         evt.message.split.map {|message_text|
-          converted_text = ROMAJI_CONVERSION_TABLE.each_with_object(message_text) {|(k, v), acc|
+          converted_text = ROMAJI_CONVERSION_TABLE.inject(message_text) {|(k, v), acc|
             acc.
               gsub(/nn$/, 'n').
               gsub(/m([bmp])/, 'n\1').
