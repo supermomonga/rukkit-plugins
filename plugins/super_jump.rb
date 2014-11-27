1086980f0cb5e8cddb7dbf5293b2613c39c854ad
ujihisa <ujihisa@gmail.com>
2014-11-26 23:09:00 -0800

    let's become an idle

--- plugins/super_jump.rb
+++ plugins/super_jump.rb
@@ -38,11 +38,12 @@ module SuperJump
 
     @vertical_accelerated ||= {}
     unless @vertical_accelerated[player.name]
-      play_sound(player.location, Sound::BAT_TAKEOFF, 0.5, 0.5)
+      play_sound(player.location, Sound::BAT_IDLE, 0.5, 0.5)
       player.velocity = Vector.new(
         player.velocity.x * 2.0,
         player.velocity.y,
         player.velocity.z * 2.0)
+
       @vertical_accelerated[player.name] = true
       later sec(1) do
         @vertical_accelerated[player.name] = false
