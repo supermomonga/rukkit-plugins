d085ebdf2806d38ad02987c11c8aae317c606d14
deris0126 <deris0126@gmail.com>
2014-12-01 02:24:45 +0900

    Add rare_material_notification

---
+++ plugins/rare_material_notification.rb
@@ -0,0 +1,26 @@
+# encoding: utf-8
+
+module RareMaterialNotification
+  extend self
+  extend Rukkit::Util
+
+  def on_player_pickup_item(evt)
+    player = evt.player
+    item = evt.item
+
+    msg = case item.item_stack.type
+    when Material::DIAMOND_ORE then
+      'ダイアモンド鉱石ゲットだぜ！'
+    when Material::LAPIS_ORE then
+      'ラピスラズリ鉱石 ヽ(*´∀｀)ノ ｷｬｯﾎｰｲ!!'
+    when Material::EMERALD_ORE then
+      'エメラルド鉱石 ｷﾀﾜｧ*:.｡..｡.:*･ﾟ (n‘∀‘)ηﾟ･*:.｡..｡.:* ﾐ ☆'
+    else
+      ''
+    end
+
+    player.chat msg unless msg.empty?
+  end
+
+end
+
