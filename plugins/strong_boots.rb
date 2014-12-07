ce42ce0e24c410290d0022c32b019ad4c2bba576
ujihisa <ujihisa@gmail.com>
2014-12-07 01:22:17 -0800

    deprecated plan message

--- plugins/strong_boots.rb
+++ plugins/strong_boots.rb
@@ -6,11 +6,14 @@ module StrongBoots
   extend self
   extend Rukkit::Util
 
+  @deprecated_message = {}
   def on_entity_damage(evt)
     # log.info("on_entity_damage: #{evt}")
 
     case evt.entity
     when Player
+      player = evt.entity
+
       case evt.cause
       when EntityDamageEvent::DamageCause::FALL
         b = evt.entity.inventory.boots or return
@@ -20,6 +23,14 @@ module StrongBoots
         evt.cancelled = true
         play_sound(
           add_loc(evt.entity.location, 0, 5, 0), Sound::BAT_HURT, 0.5, 0.0)
+
+        if !@deprecated_message[player.name]
+          @deprecated_message[player.name] = true
+          player.send_message "[BOOTS] This behaviour will change in the future. Be careful."
+          later sec(3600) do # every 1 hour
+            @deprecated_message[player.name] = false
+          end
+        end
       end
     end
   end
