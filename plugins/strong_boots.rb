b654ef5ba1395f353f4feb2adc94154c20ff094a
ujihisa <ujihisa@gmail.com>
2014-12-13 01:38:12 -0800

    refactoring

--- plugins/strong_boots.rb
+++ plugins/strong_boots.rb
@@ -9,25 +9,22 @@ module StrongBoots
   @deprecated_message = {}
   def on_entity_damage(evt)
     # log.info("on_entity_damage: #{evt}")
+    player = evt.entity
+    return unless Player === player
 
-    case evt.entity
-    when Player
-      player = evt.entity
+    case evt.cause
+    when EntityDamageEvent::DamageCause::FALL
+      b = player.inventory.boots or return
 
-      case evt.cause
-      when EntityDamageEvent::DamageCause::FALL
-        b = player.inventory.boots or return
+      evt.cancelled = true
+      # play_sound(player.location, Sound::BAT_HURT, 0.5, 0.0)
+      play_sound(player.location, Sound::BURP, 0.5, 0.0)
 
-        evt.cancelled = true
-        # play_sound(player.location, Sound::BAT_HURT, 0.5, 0.0)
-        play_sound(player.location, Sound::BURP, 0.5, 0.0)
-
-        if !@deprecated_message[player.name]
-          @deprecated_message[player.name] = true
-          player.send_message "[BOOTS] This behaviour will change in the future. Be careful."
-          later sec(3600) do # every 1 hour
-            @deprecated_message[player.name] = false
-          end
+      if !@deprecated_message[player.name]
+        @deprecated_message[player.name] = true
+        player.send_message "[BOOTS] This behaviour will change in the future. Be careful."
+        later sec(3600) do # every 1 hour
+          @deprecated_message[player.name] = false
         end
       end
     end
