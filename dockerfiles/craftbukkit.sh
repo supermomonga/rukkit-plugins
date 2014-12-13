c597b83b7cae9e946dbd6393ac8fad2e114a847a
supermomonga <hi@supermomonga.com>
2014-12-14 00:32:54 +0900

    it must be ok

--- dockerfiles/craftbukkit.sh
+++ dockerfiles/craftbukkit.sh
@@ -4,7 +4,9 @@ mkdir -p /data/world_nether
 mkdir -p /data/world_the_end
 mkdir -p /data/dynmap
 mkdir -p /data/rukkit
-[ -f "/craftbukkit/plugins/rukkit/config.yml" ] || (cp /craftbukkit/plugins/rukkit/repository/config.yml.sample /data/rukkit/config.yml)
+
+[ -f "/data/rukkit/config.yml" ] || (mv /celler/config.yml /data/rukkit/config.yml)
+
 cd /craftbukkit/
 java -Xmx$1 -jar craftbukkit.jar -o true
 
