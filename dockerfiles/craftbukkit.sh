dcac638a52e69c1d6eb7f1f73427917cf7208b10
supermomonga <hi@supermomonga.com>
2014-12-13 20:33:36 +0900

    backup rukkit dir to use those files

--- dockerfiles/craftbukkit.sh
+++ dockerfiles/craftbukkit.sh
@@ -3,7 +3,7 @@ mkdir -p /data/world
 mkdir -p /data/world_nether
 mkdir -p /data/world_the_end
 mkdir -p /data/dynmap
-[ -f "/craftbukkit/plugins/rukkit/config.yml" ] || (cp /rukkit/config.yml.sample /craftbukkit/plugins/rukkit/config.yml)
+[ -f "/craftbukkit/plugins/rukkit/config.yml" ] || (cp ~/rukkit_files/config.yml.sample /craftbukkit/plugins/rukkit/config.yml)
 cd /craftbukkit/
 java -Xmx$1 -jar craftbukkit.jar -o true
 
