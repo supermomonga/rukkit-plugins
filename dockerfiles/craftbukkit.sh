4bf5c0aa7a466e0a51e6bcd05da570c09555c596
supermomonga <hi@supermomonga.com>
2014-12-13 23:17:45 +0900

    change sinatra port

--- dockerfiles/craftbukkit.sh
+++ dockerfiles/craftbukkit.sh
@@ -4,7 +4,7 @@ mkdir -p /data/world_nether
 mkdir -p /data/world_the_end
 mkdir -p /data/dynmap
 mkdir -p /data/rukkit
-[ -f "/craftbukkit/plugins/rukkit/config.yml" ] || (cp /rukkit/config.yml.sample /data/rukkit/config.yml)
+[ -f "/craftbukkit/plugins/rukkit/config.yml" ] || (cp /craftbukkit/plugins/rukkit/config.yml.sample /data/rukkit/config.yml)
 cd /craftbukkit/
 java -Xmx$1 -jar craftbukkit.jar -o true
 
