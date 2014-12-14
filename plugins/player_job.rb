0dbdc9c621b41ad1dacca78ebb6eb011394637fd
deris0126 <deris0126@gmail.com>
2014-12-14 23:21:27 +0900

    refactoring divide module knight and fighter

--- plugins/player_job.rb
+++ plugins/player_job.rb
@@ -3,7 +3,7 @@ require 'set'
 import 'org.bukkit.entity.Player'
 import 'org.bukkit.Material'
 
-module PlayerJob
+module PlayerJobKnight
   extend self
   extend Rukkit::Util
 
@@ -13,22 +13,17 @@ module PlayerJob
 
     # become job with 50% of probability
     @be_knights ||= Set.new
-    @be_fighters ||= Set.new
     @be_knights.add(player) if random.rand(100) < 50
-    @be_fighters.add(player) if random.rand(100) < 50
     broadcast "#{player.name}さんが剣士になりました(剣の攻撃と防御が強くなります!)" if @be_knights.include?(player)
-    broadcast "#{player.name}さんが武闘家になりました(装備なし、手持ちなしで攻撃と防御が強くなります!)" if @be_fighters.include?(player)
   end
 
   def on_player_quit(evt)
     player = evt.player
     @be_knights.delete(player)
-    @be_fighters.delete(player)
   end
 
   def on_entity_damage_by_entity(evt)
     act_as_knight(evt)
-    act_as_fighter(evt)
   end
 
   def act_as_knight(evt)
@@ -45,6 +40,34 @@ module PlayerJob
         evt.set_damage(damage_after_defend(evt.get_damage, 3.0))
       end
     end
+  end
+
+  def damage_after_defend(damage, defend)
+    damage * (1.0 - (0.08 * defend))
+  end
+end
+
+module PlayerJobFighter
+  extend self
+  extend Rukkit::Util
+
+  def on_player_join(evt)
+    player = evt.player
+    random = Random.new
+
+    # become job with 50% of probability
+    @be_fighters ||= Set.new
+    @be_fighters.add(player) if random.rand(100) < 50
+    broadcast "#{player.name}さんが武闘家になりました(装備なし、手持ちなしで攻撃と防御が強くなります!)" if @be_fighters.include?(player)
+  end
+
+  def on_player_quit(evt)
+    player = evt.player
+    @be_fighters.delete(player)
+  end
+
+  def on_entity_damage_by_entity(evt)
+    act_as_fighter(evt)
   end
 
   def act_as_fighter(evt)
