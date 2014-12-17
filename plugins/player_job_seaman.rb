a256b7606090add1315ff2910265091db06e020d
deris0126 <deris0126@gmail.com>
2014-12-18 03:29:13 +0900

    add seaman job

---
+++ plugins/player_job_seaman.rb
@@ -0,0 +1,17 @@
+# encoding: utf-8
+import 'org.bukkit.event.entity.EntityDamageEvent'
+
+module PlayerJobSeaman
+  extend self
+  extend PlayerJob
+
+  login_message do |evt|
+    "#{evt.player.name}さんが海士になりました(海に入っても溺れない)"
+  end
+
+  def on_entity_damage(evt)
+    return unless has_job?(evt.entity)
+
+    evt.cancelled = true if evt.cause == EntityDamageEvent::DamageCause::DROWNING
+  end
+end
