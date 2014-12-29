0e47620eb9111f6416f3c8e73f7b947064881b2a
ujihisa <ujihisa@gmail.com>
2014-12-29 09:12:04 -0800

    hadashi fighter

--- plugins/player_job_fighter.rb
+++ plugins/player_job_fighter.rb
@@ -6,7 +6,7 @@ module PlayerJobFighter
   extend PlayerJob
 
   login_message do |evt|
-    "#{evt.player.name}さんが武闘家になりました(装備なし、手持ちなしで攻撃と防御が強くなります!)"
+    "#{evt.player.name}さんが武闘家になりました(装備なし、手持ちなしで攻撃と防御が強くなります。裸足のときのみノーダメージ着地可能)"
   end
 
   def on_entity_damage_by_entity(evt)
@@ -22,6 +22,13 @@ module PlayerJobFighter
       if has_job?(damagee) && PlayerUtil.naked?(damagee)
         evt.damage = damage_after_defend(evt.damage, 10.0)
       end
+    end
+
+    case evt.cause
+    when EntityDamageEvent::DamageCause::FALL
+      return if player.inventory.boots
+      evt.cancelled = true
+      play_sound(player.location, Sound::BAT_HURT, 0.5, 0.0)
     end
   end
 
