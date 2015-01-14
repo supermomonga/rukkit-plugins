ba0bfaee974e27747c29bd5d4f928e5b77c5aa74
deris0126 <deris0126@gmail.com>
2015-01-14 09:16:56 +0900

    add plant booster

--- plugins/player_job_farmer.rb
+++ plugins/player_job_farmer.rb
@@ -20,7 +20,7 @@ module PlayerJobFarmer
   ]
 
   login_message do |evt|
-    "#{evt.player.name}さんが農家になりました(空気中で鍬を振ると周りの作物が成長。範囲耕しが可能)"
+    "#{evt.player.name}さんが農家になりました(空気中で鍬を振ると周りの作物が成長。範囲耕し、範囲種植が可能)"
   end
 
   def on_player_interact(evt)
@@ -30,29 +30,53 @@ module PlayerJobFarmer
     world = player.world
 
     return unless has_job?(player)
-    return unless MaterialUtil.hoe?(material)
 
-    case action
-    when Action::LEFT_CLICK_AIR
-      return unless rand(3) == 0
-      around_square_loc(player.location).each do |(x, y, z)|
-        block = world.get_block_at(x, y, z)
-        state = block.state
-        case state.data
-        when Crops
-          if state.data.state != CropState::RIPE
-            next_state = @crop_state[@crop_state.index(state.data.state) + 1]
-            state.data.state = next_state
-            state.update
+    if MaterialUtil.hoe?(material)
+      case action
+      when Action::LEFT_CLICK_AIR
+        return unless rand(3) == 0
+        around_square_loc(player.location).each do |(x, y, z)|
+          block = world.get_block_at(x, y, z)
+          state = block.state
+          case state.data
+          when Crops
+            if state.data.state != CropState::RIPE
+              next_state = @crop_state[@crop_state.index(state.data.state) + 1]
+              state.data.state = next_state
+              state.update
+            end
           end
         end
+      when Action::RIGHT_CLICK_BLOCK
+        clicked_block = evt.clicked_block
+        return unless [Material::DIRT, Material::GRASS].include?(clicked_block.type)
+        around_square_loc(clicked_block.location).each do |(x, y, z)|
+          block = world.get_block_at(x, y, z)
+          block.type = Material::SOIL if [Material::DIRT, Material::GRASS].include?(block.type)
+        end
       end
-    when Action::RIGHT_CLICK_BLOCK
-      clicked_block = evt.clicked_block
-      return unless [Material::DIRT, Material::GRASS].include?(clicked_block.type)
-      around_square_loc(clicked_block.location).each do |(x, y, z)|
-        block = world.get_block_at(x, y, z)
-        block.type = Material::SOIL if [Material::DIRT, Material::GRASS].include?(block.type)
+    elsif material == Material::SEEDS
+      case action
+      when Action::RIGHT_CLICK_BLOCK
+        return unless player.item_in_hand.type == Material::SEEDS
+        clicked_block = evt.clicked_block
+        upper_loc = clicked_block.location.clone
+        upper_loc.y += 1
+        upper_block = world.get_block_at(upper_loc)
+        return unless clicked_block.type == Material::SOIL && upper_block.type == Material::AIR
+        evt.cancelled = true
+        around_square_loc(clicked_block.location).each do |(x, y, z)|
+          block = world.get_block_at(x, y, z)
+          upper_block = world.get_block_at(x, y+1, z)
+          if block.type == Material::SOIL && upper_block.type == Material::AIR
+            upper_block.type = Material::CROPS
+            if player.item_in_hand.amount == 1
+              player.item_in_hand = ItemStack.new(Material::AIR)
+              return
+            end
+            player.item_in_hand.amount -= 1
+          end
+        end
       end
     end
   end
