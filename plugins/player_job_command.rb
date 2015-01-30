017b51f92ba5f5a5205250b21f2b05db1f58e515
deris0126 <deris0126@gmail.com>
2015-01-30 23:26:43 +0900

    refactor

--- plugins/player_job_command.rb
+++ plugins/player_job_command.rb
@@ -11,9 +11,8 @@ module PlayerJobCommand
     args = args.to_a
     case args.shift
     when 'player-job'
-      arg2 = args.shift
-      case
-      when arg2 == 'id'
+      case args.shift
+      when 'id'
         name = args.shift
         return unless name
         player = sender.world.players.find { |p| p.name ==  name }
@@ -23,19 +22,19 @@ module PlayerJobCommand
         }.each { |job|
           sender.send_message(job.detail)
         }
-      when arg2 == 'me'
+      when 'me'
         PlayerJob.select { |job|
           job.has_job?(sender) && job.respond_to?(:detail)
         }.each { |job|
           sender.send_message(job.detail)
         }
-      when arg2 == 'list'
+      when 'list'
         PlayerJob.select { |job|
           job.respond_to?(:detail)
         }.each { |job|
           sender.send_message(job.detail)
         }
-      when arg2 == 'help'
+      when 'help'
         broadcast '/rukkit player-job id [user id] --'
         broadcast '        show current job of user specified by id'
         broadcast '/rukkit player-job me -- show current job of youself'
