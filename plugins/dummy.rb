202408b9f5e1e00e14b2d58a6093387073edaf93
ujihisa <ujihisa@gmail.com>
2015-01-31 16:26:49 -0800

    good-bye, dummy eval

--- plugins/dummy.rb
+++ plugins/dummy.rb
@@ -9,16 +9,6 @@ module Dummy
 
     args = args.to_a
     case args.shift
-    when 'eval'
-      # very dangerous!
-      later(0) do
-        begin
-          # how to handle double-space?
-          sender.send_message(eval(args.join(' ')).inspect)
-        rescue => e
-          sender.send_message(e.inspect)
-        end
-      end
     when 'what-time'
       msg = "#{sender.name} (what-time) => #{Time.now.to_s}"
       broadcast msg
