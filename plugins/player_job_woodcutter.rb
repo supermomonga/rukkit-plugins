47198ff3e369c1be48f6d5d762dabe6ad36a7466
supermomonga <hi@supermomonga.com>
2016-07-05 12:35:19 +0900

    move kickory skill to woodcutter

--- plugins/player_job_woodcutter.rb
+++ plugins/player_job_woodcutter.rb
@@ -45,4 +45,32 @@ module PlayerJobWoodcutter
       end
     end
   end
+
+  # If you break a log with an axe with at least 1 enchantment,
+  # it will also break logs below/above. If will chain as long as you keep your axe.
+  def on_block_break(evt)
+    return if evt.cancelled
+    woodlog = evt.block
+    return unless [Material::LOG, Material::LOG_2].include?(woodlog.type)
+    player = evt.player
+    axe = player.item_in_hand
+    return unless [Material::DIAMOND_AXE, Material::GOLD_AXE, Material::IRON_AXE, Material::STONE_AXE, Material::WOOD_AXE].include?(axe.type)
+    return if axe.enchantments.empty?
+
+    [*0..1].each do |ydiff|
+      [*-1..1].each do |xdiff|
+        [*-1..1].each do |zdiff|
+          block = add_loc(woodlog.location, xdiff, ydiff, zdiff).block
+
+          later sec(0.5) do
+            if player.valid? && [Material::LOG, Material::LOG_2].include?(block.type)
+              evt2 = BlockBreakEvent.new(block, player)
+              Bukkit.plugin_manager.call_event(evt2)
+              block.break_naturally(axe)
+            end
+          end
+        end
+      end
+    end
+  end
 end
